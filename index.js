e50ca5cbf648dd78de73be517fa36a72ff5b76aa
Minji <321963741@naver.com>
2022-01-15 13:53:34 +0900

    type define

---
+++ index.js
@@ -0,0 +1,7 @@
+var person = 'minji';
+var number = 2;
+var minji = function (name, number) {
+    person = name;
+    console.log(number, person);
+};
+minji('gan', 3);
---
+++ index.ts
@@ -0,0 +1,9 @@
+let person = 'minji';
+let number = 2;
+
+const minji = (name: string, number: number) => {
+  person = name;
+  console.log(number, person);
+};
+
+minji('gan', 3);
