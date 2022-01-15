f9c3f1f5d4812f236cddf221c02f104bdcf26327
Minji <321963741@naver.com>
2022-01-15 20:00:19 +0900

    class readonly, private,public

--- public/class.js
+++ public/class.js
@@ -1,5 +1,20 @@
 "use strict";
+// readonly => 수정만 불가능하고 읽기만 가능
+// private => 클로저 투척! class안에서만 읽고, 쓰는거 가능
+// public => default값
+// 1. 그냥 없이 사용
 class Diary {
+    constructor(writer, detail, date) {
+        this.writer = writer;
+        this.detail = detail;
+        this.date = date;
+    }
+    write() {
+        console.log(`${this.writer} wrote diary for today ${this.date} in ${this.detail}`);
+    }
+}
+//2.  readonly,private,public 사용
+class Diary1 {
     constructor(writer, detail, date) {
         this.writer = writer;
         this.detail = detail;
--- src/class.ts
+++ src/class.ts
@@ -1,3 +1,8 @@
+// readonly => 수정만 불가능하고 읽기만 가능
+// private => 클로저 투척! class안에서만 읽고, 쓰는거 가능
+// public => default값
+
+// 1. 그냥 없이 사용
 class Diary {
   writer: string;
   detail: string;
@@ -7,6 +12,20 @@ class Diary {
     this.detail = detail;
     this.date = date;
   }
+  write() {
+    console.log(
+      `${this.writer} wrote diary for today ${this.date} in ${this.detail}`
+    );
+  }
+}
+//2.  readonly,private,public 사용
+class Diary1 {
+  constructor(
+    readonly writer: string,
+    private detail: string,
+    public date: string
+  ) {}
+
   write() {
     console.log(
       `${this.writer} wrote diary for today ${this.date} in ${this.detail}`
