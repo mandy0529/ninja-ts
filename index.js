5e73172d045904f011e62f011bab32f1277d8513
Minji <321963741@naver.com>
2022-01-15 14:06:57 +0900

    array object typescript

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
 var person = 'minji';
 var number = 2;
+var groups = ['minji', 'gan', true, 3];
+var objects = {
+    name: 'logger',
+    age: 26,
+    job: true
+};
 var minji = function (name, number) {
     person = name;
     console.log(number, person);
 };
 minji('gan', 3);
+groups.push('cooper', false, 5);
+groups[0] = 'type';
+console.log(groups);
+objects.name = 'minji';
+objects.age = 16;
+objects.job = true;
+console.log(objects, '1');
+objects = {
+    name: 'gan',
+    age: 26,
+    job: false
+};
+console.log(objects, '2');
--- index.ts
+++ index.ts
@@ -1,5 +1,11 @@
 let person = 'minji';
 let number = 2;
+let groups = ['minji', 'gan', true, 3];
+let objects = {
+  name: 'logger',
+  age: 26,
+  job: true,
+};
 
 const minji = (name: string, number: number) => {
   person = name;
@@ -7,3 +13,18 @@ const minji = (name: string, number: number) => {
 };
 
 minji('gan', 3);
+
+groups.push('cooper', false, 5);
+groups[0] = 'type';
+console.log(groups);
+
+objects.name = 'minji';
+objects.age = 16;
+objects.job = true;
+console.log(objects, '1');
+objects = {
+  name: 'gan',
+  age: 26,
+  job: false,
+};
+console.log(objects, '2');
